ca409570b68241d4e6da544092dd6b4c15a2c55b
pf-github-pl <pf.github@gmail.com>
2022-06-09 14:47:36 +0200

    initial commit for pub

---
+++ README.md
@@ -0,0 +1,8 @@
+# My aliases for bash/zsh shell
+
+## You can add them by running `load_aliases.sh`
+
+## Alternatively follow this steps:
+- copy `myaliases.txt` to `.myaliases` in your home directory
+- add ref to the file in `.bashrc` : `source /path_to_your_home_dir/.myaliases`
+- execute `source /path_to_your_home_dir/.bashrc` or restart your shell to load changes in `.bashrc` file
---
+++ load_aliases.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Exit script if not running on Linux
+if [[ ! "$OSTYPE" =~ ^linux ]]; then
+    echo "Your OS is not supported."
+	echo "Finishing..."
+	exit 0
+fi
+
+# Copy aliases to hidden file in user home directory if allowed
+if test /home/$USER/.myaliases; then  
+	echo "The file already exists. Do you want to override? [y/N]" 
+	read over
+	if [[ ${over^} != "Y" ]]; then
+		echo "Finishing... "
+		exit 0
+	fi
+fi
+
+cp ./myaliases.txt /home/$USER/.myaliases
+echo "The file copied to /home/$USER/.myaliases"
+
+# Choose your shell
+echo "Choose your shell[1-3]:
+1) bash
+2) zsh
+3) other"
+read shell
+
+# Declare path to shell config or exit
+case $shell in
+	1) path="/home/$USER/.bashrc" ;;
+	2) path="/home/$USER/.zshrc" ;;
+	3) echo -e "Not supported\nFinishing..." && exit 0 ;;
+esac
+
+# Add ref to aliases in shell config if not exists already
+echo "Adding reference to $path"
+grep -q "/home/$USER/.myaliases 2>/dev/null" ~/.bashrc || echo -e "# MY ALIASES\nsource /home/$USER/.myaliases 2>/dev/null" >> $path
+echo "DONE!!"
+
+# Reload your shell
+source $path
+test $SHELL != "" && exec $SHELL || echo -e "Please reload your shell or execute following command manually: \"source $path\""
